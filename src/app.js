1a305e01b647ee21e98fa6e751a7591e27b35f43
Beatriz Herranz <167246883+BeatrizHerranz@users.noreply.github.com>
2024-06-01 11:38:57 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -9,3 +9,24 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+let who = ["The dog", "My grandma", "The mailman", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "my phone", "the car"];
+let when = [
+  "before the class",
+  "when I was sleeping",
+  "while I was exercising",
+  "during my lunch",
+  "while I was praying"
+];
+
+let randomWho = who[Math.floor(Math.random() * who.length)];
+let randomAction = action[Math.floor(Math.random() * action.length)];
+let randoWhat = what[Math.floor(Math.random() * what.length)];
+let randoWhen = when[Math.floor(Math.random() * when.length)];
+
+document.getElementById(
+  "excuse"
+).innerHTML = `${randomWho} ${randomAction} ${randoWhat} ${randoWhen}`;
+//write your code here
+console.log("Hello Rigo from the console!");
